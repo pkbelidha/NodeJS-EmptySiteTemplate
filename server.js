4bd32d96877a1de22a1050901e140a7d0c4b27ad
pkbelidha <pkbelidha@gmail.com>
2019-01-18 15:25:19 -0800

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var http = require('http');
 http.createServer(function (req, res) {
     
     res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write("The date and time are currently: " + dt.myDateTime());
     res.end('Hello, Prashanth Kumar Belidha!');
     
 }).listen(process.env.PORT || 8080);
