9f453e6a556b993261c2d79fe2c0ea7a875ed21c
pkbelidha <pkbelidha@gmail.com>
2019-01-18 15:35:32 -0800

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 http.createServer(function (req, res) {
 	
-	      res.writeHead(200, {'Content-Type': 'text/html'});
+	      res.writeHead(200, { 'Content-Type': 'text/html' });
               res.write("The date and time is currently: " + dt.myDateTime());
               res.end();
 	}).listen(process.env.PORT || 8080);
