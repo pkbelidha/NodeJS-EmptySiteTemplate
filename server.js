f19ec72d6912e31d227c5731e1bdf9f58f995015
pkbelidha <pkbelidha@gmail.com>
2019-01-18 16:37:29 -0800

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ var dt = require('node-datetime');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write("The date and time is currently: " + dt(now, "longTime", true));
-  res.end();
+  res.end('Hello, Prashanth Kumar Belidha!');
 	}).listen(process.env.PORT || 8080);
 
